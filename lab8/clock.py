c16e67d77bb1fbb0f855dcd1842be73a5f99ce0b
alphy1 <42415340+alphy1@users.noreply.github.com>
2020-09-25 13:16:47 +0300

    Update to Vector Clock

--- lab8/clock.py
+++ lab8/clock.py
@@ -2,55 +2,58 @@ from multiprocessing import Process, Pipe
 from os import getpid
 from datetime import datetime
 
-# def local_time(counter):
-#     return '(LAMPORT_TIME={}, LOCAL_TIME={})'.format(counter,datetime.now())
-
 def calc_recv_timestamp(recv_time_stamp, counter):
-    return max(recv_time_stamp, counter) + 1
+    for i in range(len(counter)):
+        counter[i] = max(recv_time_stamp[i], counter[i])
+    return counter
 
 def event(pid, counter):
-    counter += 1
-    print('Something happened in {}!'.format(pid))
+    counter[pid] += 1
     return counter
 
 def send_message(pipe, pid, counter):
-    counter += 1
+    counter[pid] += 1
     pipe.send(('Empty shell', counter))
-    print('Message sent from ' + str(pid))
     return counter
 
 def recieve_message(pipe, pid, counter):
     message, timestamp = pipe.recv()
     counter = calc_recv_timestamp(timestamp, counter)
-    print('Message received at ' + str(pid))
     return counter
 
 def process1(pipe12):
-    pid = getpid()
-    print('Process 1 has ID ', pid)
-    counter = 0
+    pid = 0
+    counter = [0, 0, 0]
+    counter = send_message(pipe12, pid, counter)
+    counter = event(pid, counter)
     counter = event(pid, counter)
     counter = send_message(pipe12, pid, counter)
-    counter  = event(pid, counter)
+    counter = event(pid, counter)
     counter = recieve_message(pipe12, pid, counter)
-    counter  = event(pid, counter)
+    counter = recieve_message(pipe12, pid, counter)
+    print('Process 1: ', counter)
 
 def process2(pipe21, pipe23):
-    pid = getpid()
-    print('Process 2 has ID ', pid)
-    counter = 0
-    counter = send_message(pipe23, pid, counter)
+    pid = 1
+    counter = [0, 0, 0]
+    counter = recieve_message(pipe23, pid, counter)
     counter = recieve_message(pipe23, pid, counter)
     counter = recieve_message(pipe21, pid, counter)
+    counter = event(pid, counter)
+    counter = send_message(pipe23, pid, counter)
+    counter = recieve_message(pipe21, pid, counter)
     counter = send_message(pipe21, pid, counter)
-    
+    counter = send_message(pipe21, pid, counter)
+    print('Process 2: ', counter)
 
 def process3(pipe32):
-    pid = getpid()
-    print('Process 3 has ID ', pid)
-    counter = 0
-    counter = recieve_message(pipe32, pid, counter)
+    pid = 2
+    counter = [0, 0, 0]
     counter = send_message(pipe32, pid, counter)
+    counter = event(pid, counter)
+    counter = send_message(pipe32, pid, counter)
+    counter = recieve_message(pipe32, pid, counter)
+    print('Process 3: ', counter)
 
 if __name__ == '__main__':    
     pipe12, pipe21 = Pipe()
